083066c02f0e96582c5212bc0da88a87f568d44f
JackZouShao <jackzouyan@gmail.com>
2024-08-19 20:45:13 +0900

    第一次提交

---
+++ Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author yanzou
+ * @version V1.0.0
+ * @date 2024/8/19
+ * @description
+ **/
+public class Demo{
+
+    public static void main(String[] args){
+        System.out.println("Hello World 1232");
+    }
+}
