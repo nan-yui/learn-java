3b06ea82277e201baa1bc7caaf0ddad6b13c2238
JackZouShao <jackzouyan@gmail.com>
2024-08-21 19:51:24 +0900

    第十七课 制表符

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,34 +5,35 @@ public class HelloWorld {
         会把小括号里面的内容进行输出打印*/
         System.out.println("Hello World");
 
-
+        String publi1c = "123";
 
         //常见的数据代码如何书写？
+        int a = 123;
 
         //整数
         System.out.println(-666);
+
         //小数
         System.out.println(5.21);
+
         //字符串
         System.out.println("黑马程序员");
+
         //字符
         System.out.println('女');
+
         //布尔
         System.out.println(true);
         System.out.println(false);
+
         //空
         //细节:null不能字直接打印，只能用字符串的形式进行打印
         System.out.println("null");
 
 
-
-
+        // 第17课 制表符
         System.out.println("name"+'\t'+"age");
         System.out.println("tom"+"\t"+"23");
-
-
-
-
 
 
 
