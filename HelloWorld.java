921aa0b774cf19e3c968a077295843c2504276bb
nan-yui <nanyui51@hotmail.com>
2024-08-21 19:41:39 +0900

    2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,41 @@
 public class HelloWorld {
+    //叫做main方法，表示程序的主路口
     public static void main(String[] args) {
+        /*叫做输出语句（打印语句）
+        会把小括号里面的内容进行输出打印*/
         System.out.println("Hello World");
+
+
+
+        //常见的数据代码如何书写？
+
+        //整数
+        System.out.println(-666);
+        //小数
+        System.out.println(5.21);
+        //字符串
+        System.out.println("黑马程序员");
+        //字符
+        System.out.println('女');
+        //布尔
+        System.out.println(true);
+        System.out.println(false);
+        //空
+        //细节:null不能字直接打印，只能用字符串的形式进行打印
+        System.out.println("null");
+
+
+
+
+        System.out.println("name"+'\t'+"age");
+        System.out.println("tom"+"\t"+"23");
+
+
+
+
+
+
+
 
     }
 
